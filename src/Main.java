feed3f8bbab2c5da5f269cec667ef6e8a0b6c2e3
Thibaut Méchain <116610147+TibMec@users.noreply.github.com>
2025-09-24 08:44:37 -0400

    Update Main.java
    -message ajouté

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Pasta linguini = new Pasta("Linguini",true,10);
+        System.out.println("Vous avez selectionné ce type de pâtes:");
         System.out.println(linguini);
     }
 }
