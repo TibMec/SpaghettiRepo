abe72dc71bc7a1a35b0f5c9c3f932c1d7f90b2d2
Thibaut Méchain <116610147+TibMec@users.noreply.github.com>
2025-09-24 08:50:37 -0400

    Update Main.java
    - message de validation ajouté

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Pasta linguini = new Pasta("Linguini",true,10);
         System.out.println(linguini);
+        System.out.println("Très bon choix !");
     }
 }
